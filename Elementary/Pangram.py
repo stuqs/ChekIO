e2feef1301b81ce668eba3f8168763442f407fe8
stuqs <nanorhist@ukr.net>
2015-09-24 18:12:31 +0300

    Add 2 tasks from Elementary

---
+++ Elementary/Pangram.py
@@ -0,0 +1,12 @@
+def check_pangram(text):
+    # TODO incorect algoritgm
+    for letter in text:
+        if letter.isalpha() and text.count(letter) > 1:
+            return False
+    return True
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
+    assert not check_pangram("ABCDEF"), "ABC"
+    assert check_pangram("Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!"), "Bored?"
